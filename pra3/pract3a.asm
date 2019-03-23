6ed5a677dc90caf66f55727c326b29a077710add
Alejandro Santorum <alejandro.santorum@gmail.com>
2019-03-23 19:42:13 +0100

    ejercicio 3a (computeControlDigit) posiblemente terminado, no comprobado

--- pra3/pract3a.asm
+++ pra3/pract3a.asm
@@ -1,3 +1,11 @@
+;**************************************************************************
+;   Autores:
+;       Rafael Sanchez Sanchez - rafael.sanchezs@estudiante.uam.es
+;       Alejandro Santorum Varela - alejandro.santorum@estudiante.uam.es
+;   Pareja: 16
+;   Practica 3 Sistemas Basados en Microprocesadores
+;**************************************************************************
+;**************************************************************************
 PRAC3A SEGMENT BYTE PUBLIC 'CODE'
 PUBLIC _computeControlDigit
 ASSUME CS: PRAC3A
@@ -5,12 +13,14 @@ ASSUME CS: PRAC3A
 _computeControlDigit PROC FAR
     PUSH BP
     MOV BP, SP
-
+	
+	; PRESERVANDO VALORES DE LOS REGISTROS
     PUSH DS, BX, AX
-
+	
     MOV BX, [BP+6]
     MOV DS, [BP+8]
-
+	
+	; SUMANDO LOS NUMEROS DE POSICIONES IMPARES
     MOV AX, 0
     ADD AX, [BX]
     ADD AX, 2[BX]
@@ -18,7 +28,9 @@ _computeControlDigit PROC FAR
     ADD AX, 6[BX]
     ADD AX, 8[BX]
     ADD AX, 10[BX]
-
+	
+	; MULTIPLICANDO POR 3 LOS NUMEROS DE POSICIONES
+	; PARES Y SUMANDOLOS A LO ANTERIOR
     ADD AX, 1[BX]
     ADD AX, 1[BX]
     ADD AX, 1[BX]
@@ -42,9 +54,33 @@ _computeControlDigit PROC FAR
     ADD AX, 11[BX]
     ADD AX, 11[BX]
     ADD AX, 11[BX]
-
-    POP AX, BX, DS, BP
-    RET
+	
+	; DIVIDIMOS AX POR 10
+	SUB AX, 10
+	SUB AX, 10
+	SUB AX, 10
+	SUB AX, 10
+	SUB AX, 10
+	SUB AX, 10
+	SUB AX, 10
+	SUB AX, 10
+	SUB AX, 10
+	SUB AX, 10
+	
+	; SI AX ERA UN MULTIPLO DE 10, ENTONCES YA
+	; TENEMOS UN 0 EN AX (EL DIGITO DE CONTROL)
+	CMP AX, 0
+	JZ FIN:
+	; POR EL CONTRARIO, TENEMOS QUE CALCULAR
+	; 10 - EL RESTO DE LA DIVISION Y GUARDARLO
+	; EN AX
+	XCHG AX, BX ; ax <-> bx (swap)
+	MOV AX, 10  ; ax <- 10
+	SUB AX, BX  ; ax := 10 - (RESTO DIV /10)
+	
+	FIN					;;; !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    POP AX, BX, DS, BP ;;;; <== CREO QUE NO HAY QUE PRESERVAR AX, PORQUE AHÍ ES POR DONDE SE DEVUELVE EL DIGITO DE CONTROL
+    RET					;;; !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 _computeControlDigit ENDP
 
 PRAC3A ENDS ; FIN DEL SEGMENTO DE CODIGO
